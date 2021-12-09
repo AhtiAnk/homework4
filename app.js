5ece7b8d0d67fd31ebe60883750edffdb506941b
AhtiAnk <ahtiankro@gmail.com>
2021-12-09 19:51:06 +0200

    posts page

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express'); 
 const pool = require('./database'); 
+const cors = require('cors');
  
 const app = express(); 
  
 app.set('view engine', 'ejs'); 
  
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); 
+app.use(express.static('Public'));
  
 app.listen(3000, () => { 
     console.log("Server is listening to port 3000") 
+});
+
+app.get('/', async(req, res) => { 
+    try { 
+        console.log("get posts request has arrived"); 
+        const posts = await pool.query( 
+            "SELECT * FROM posts" 
+        ); 
+        res.render('posts', { posts: posts.rows });  
+    } catch (err) { 
+        console.error(err.message); 
+    } 
 });
