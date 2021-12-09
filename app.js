54e38943bde37afbe1670ded1117d653fd7c4c62
AhtiAnk <ahtiankro@gmail.com>
2021-12-09 22:00:23 +0200

    add new post

--- app.js
+++ app.js
@@ -15,14 +15,32 @@ app.listen(3000, () => {
     console.log("Server is listening to port 3000") 
 });
 
+app.get('/addnewpost', (req, res) => {
+        res.render('addnewpost'); 
+    })
+
 app.get('/', async(req, res) => { 
     try { 
         console.log("get posts request has arrived"); 
         const posts = await pool.query( 
             "SELECT * FROM posts" 
         ); 
-        res.render('posts', { posts: posts.rows });  
+        res.render('posts', { posts: posts.rows });
     } catch (err) { 
         console.error(err.message); 
     } 
 });
+
+app.post('/', async(req, res) => { 
+    try { 
+        const post = req.body; 
+        console.log(post); 
+        console.log("add new post request has arrived");
+        const newpost = await pool.query( 
+            "INSERT INTO posts(comment, time) values ($1, $2) RETURNING*", [post.comment, post.time] 
+        ); 
+        res.redirect('/'); 
+    } catch (err) { 
+        console.error(err.message) 
+    } 
+}); 
