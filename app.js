6da21623e1ccc5867100ebb51146a40b73428e10
merili132 <merilikovask0@gmail.com>
2021-12-10 12:38:22 +0200

    singlepost and 404 ejs

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors()); 
 app.use(express.static('Public'));
+
+/*  1.4?
+    app.get('*', function(req, res){
+    res.send('what???', 404);
+  });*/
  
 app.listen(3000, () => { 
     console.log("Server is listening to port 3000") 
@@ -26,3 +31,31 @@ app.get('/', async(req, res) => {
         console.error(err.message); 
     } 
 });
+
+/*app.get('/:id', async(req, res) => {
+    try {
+    console.log("get a post request has arrived");
+    const posts = await pool.query(
+    "SELECT * FROM posts WHERE id = $1", [id]
+    );
+    res.render('posts', { posts: posts.rows });
+    res.json(posts.rows);
+    } catch (err) {
+    onsole.error(err.message);
+    }
+   });*/
+
+app.delete('/posts/:id', async(req, res) => {
+    try {
+    const { id } = req.params;
+    const post = req.body;
+    console.log("delete a post request has arrived");
+    const deletepost = await pool.query(    
+    "DELETE FROM posts WHERE id = $1", [id]
+    );
+    res.json(post);
+    } catch (err) {
+    console.error(err.message);
+    }
+   });
+   
