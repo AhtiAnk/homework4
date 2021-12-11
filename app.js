e0ca023e595de89b2c0ab75fc26378bf45cf2f68
AhtiAnk <ahtiankro@gmail.com>
2021-12-11 18:43:09 +0200

    add new post creates date automatically and like button which doesnt disable yet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const pool = require('./database'); 
 const cors = require('cors');
+const { identity } = require('lodash');
  
 const app = express(); 
  
@@ -65,7 +66,6 @@ app.get('/', async(req, res) => {
 
 app.delete('/posts/:id', async(req, res) => {
     try {
-    const { id } = req.params;
     const post = req.body;
     console.log("delete a post request has arrived");
     const deletepost = await pool.query(    
@@ -77,15 +77,38 @@ app.delete('/posts/:id', async(req, res) => {
     }
    });
    
-app.post('/', async(req, res) => { 
+app.post('/addnewpost', async(req, res) => { 
     try { 
         const post = req.body; 
         console.log(post); 
         console.log("add new post request has arrived");
+        let currentdate = new Date(); 
+        let datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        console.log(datetime);
         const newpost = await pool.query( 
-            "INSERT INTO posts(comment, time) values ($1, $2) RETURNING*", [post.comment, post.time] 
+            "INSERT INTO posts(comment, time) values ($1, $2) RETURNING*", [post.comment, datetime] 
         ); 
         res.redirect('/'); 
+    } catch (err) { 
+        console.error(err.message) 
+    } 
+});
+
+app.post('/', async(req, res) => { 
+    try { 
+        console.log(req.body); 
+        const post = req.body; 
+        console.log("add a like request")
+        let likes = parseInt(post.likes) + 1;
+        const addlike = await pool.query( 
+            "UPDATE posts SET likes = $1 WHERE id = $2", [likes, post.id] 
+        );
+        res.redirect('/');  
     } catch (err) { 
         console.error(err.message) 
     } 
